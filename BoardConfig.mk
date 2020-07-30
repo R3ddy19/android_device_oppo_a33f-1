83961f127f8d7a0fd7f41801ebb023ed03d578f9
Sumit reddy <41549877+R3ddy19@users.noreply.github.com>
2020-07-30 18:06:14 +0530

    Update BoardConfig.mk
    # Flags
    -TARGET_GLOBAL_CFLAGS += -mfpu=neon -mfloat-abi=softfp
    -TARGET_GLOBAL_CPPFLAGS += -mfpu=neon -mfloat-abi=softfp
    -COMMON_GLOBAL_CFLAGS += -DREFRESH_RATE=60  
    # Flags
    +#TARGET_GLOBAL_CFLAGS += -mfpu=neon -mfloat-abi=softfp
    +#TARGET_GLOBAL_CPPFLAGS += -mfpu=neon -mfloat-abi=softfp
    +#COMMON_GLOBAL_CFLAGS += -DREFRESH_RATE=60

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -17,9 +17,9 @@ TARGET_PLATFORM_DEVICE_BASE := /devices/soc.0/
 #TARGET_CPU_VARIANT := cortex-a7
 
 # Flags
-TARGET_GLOBAL_CFLAGS += -mfpu=neon -mfloat-abi=softfp
-TARGET_GLOBAL_CPPFLAGS += -mfpu=neon -mfloat-abi=softfp
-COMMON_GLOBAL_CFLAGS += -DREFRESH_RATE=60  
+#TARGET_GLOBAL_CFLAGS += -mfpu=neon -mfloat-abi=softfp
+#TARGET_GLOBAL_CPPFLAGS += -mfpu=neon -mfloat-abi=softfp
+#COMMON_GLOBAL_CFLAGS += -DREFRESH_RATE=60  
 
 # Architecture
 #TARGET_ARCH := arm64
